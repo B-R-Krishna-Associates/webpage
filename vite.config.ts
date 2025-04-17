ce67809c2a380a991bd062ebbdf9222f15a4508d
devRajeshThapa <devrajeshthapa@gmail.com>
2025-04-17 20:47:58 +0545

    Domain config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["brkrishna.com.np", "www.brkrishna.com.np"]
+  }
 })
